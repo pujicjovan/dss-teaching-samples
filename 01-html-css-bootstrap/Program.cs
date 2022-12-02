b72c3920e84257c79752e415fbeee531b465d9e8
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-12-02 09:05:49 +0100

    Update Program.cs

--- 01-html-css-bootstrap/Program.cs
+++ 01-html-css-bootstrap/Program.cs
@@ -16,6 +16,7 @@ namespace _03_html_css_bootstrap
             CreateHostBuilder(args).Build().Run();
         }
 
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
