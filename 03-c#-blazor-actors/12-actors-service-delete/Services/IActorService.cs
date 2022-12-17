a31f8e98fc379ba6a633ea654969d2dd23212de8
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-12-17 14:58:04 +0100

    changed

---
+++ 03-c#-blazor-actors/12-actors-service-delete/Services/IActorService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using project.Models;
+
+namespace project.Services
+{
+    public interface IActorService
+    {
+        Task<List<Actor>> GetActors(bool reload = false);
+
+        Task<List<Actor>> Add(string firstName, string lastName, string country);
+
+        Task<List<Actor>> Delete(Actor actor);
+    }
+}
