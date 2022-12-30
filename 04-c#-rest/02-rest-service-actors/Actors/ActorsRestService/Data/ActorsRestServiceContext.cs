e7ded5e136286a7b85af08c135788b4dfb765c9f
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-12-30 19:47:37 +0100

    added rest services

---
+++ 04-c#-rest/02-rest-service-actors/Actors/ActorsRestService/Data/ActorsRestServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ActorsRestService.Models;
+
+namespace ActorsRestService.Data
+{
+    public class ActorsRestServiceContext : DbContext
+    {
+        public ActorsRestServiceContext (DbContextOptions<ActorsRestServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ActorsRestService.Models.Actor> Actor { get; set; } = default!;
+    }
+}
